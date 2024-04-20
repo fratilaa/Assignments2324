83f0835f4b034936b86b6c02b7150468e6b18720
Fratila Antonia <fratilaa99@gmail.com>
2024-04-20 20:42:34 +0300

    wbs

--- WBuild/login_form/js/script.js
+++ WBuild/login_form/js/script.js
@@ -1,49 +1,43 @@
-'use strict'
-
 $(document).ready(function() {
-    $('#loginForm').submit(function(event) {
-        event.preventDefault(); // Prevent form submission
+    $('#loginForm').submit(function(e) {
+        e.preventDefault();
 
-        // Get input values
-        var username = $('#username').val();
-        var password = $('#password').val();
+        let username = $('#username').val();
+        let password = $('#password').val();
 
-        // Simulate login validation (replace with your actual validation logic)
-        var isUsernameValid = (username === 'new_user');
-        var isPasswordValid = (password === '123456789');
+       
+        let isUsernameValid = (username === 'new_user');
+        let isPasswordValid = (password === '123456789');
 
-        // Clear previous error/success indications
         $('.error').hide();
         $('.success').removeClass('success');
         $('.form-control').removeClass('border-danger border-success'); // Remove border classes
 
-        // Apply border color for username
         if (isUsernameValid) {
             $('#username').addClass('border-success');
+            $('#usernameIcon i').addClass('fa-check').removeClass('fa-times');
         } else {
             $('#username').addClass('border-danger');
+            $('#usernameIcon i').addClass('fa-times').removeClass('fa-check');
             $('#usernameError').text('*please, enter valid username').show();
         }
 
-        // Apply border color for password
         if (isPasswordValid) {
             $('#password').addClass('border-success');
+            $('#passwordIcon i').addClass('fa-check').removeClass('fa-times');
         } else {
             $('#password').addClass('border-danger');
+            $('#passwordIcon i').addClass('fa-times').removeClass('fa-check');
             $('#passwordError').text('*please, enter valid password').show();
         }
 
-        // Display success message if login is successful
         if (isUsernameValid && isPasswordValid) {
             $('#result').text('Successful login!').addClass('success');
         }
     });
 
-    // Remove error styles when user starts typing in the inputs
     $('#username, #password').keyup(function() {
         $(this).removeClass('border-danger border-success');
         $('#result').text('').removeClass('success');
     });
 });
-
-
